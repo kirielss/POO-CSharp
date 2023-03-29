a12fa90d7211d665d5f0fbdb4f7ab6dd5a8a2e94
skiriel <gabrieldllopes@gmail.com>
2023-03-28 23:02:45 -0300

    feat: Matriz Challenge

--- Matrizes/Matrizes/Program.cs
+++ Matrizes/Matrizes/Program.cs
@@ -4,11 +4,39 @@
     {
         static void Main(string[] args)
         {
-            double[,] mat = new double[2, 3];
+            int N = int.Parse(Console.ReadLine());
+            int[,] mat = new int[N,N];
+            int[] mainD = new int[N];
+            int negative = 0;
 
-            Console.WriteLine(mat.Length);
-            Console.WriteLine(mat.Rank);
-            Console.WriteLine(mat.GetLength(1));
+            for (int i = 0; i < N; i++)
+            {
+                string[] row = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < row.Length; j++)
+                {
+                    mat[j,i] = int.Parse(row[j]);
+
+                    if (j == i)
+                    {
+                        mainD[j] = int.Parse(row[j]);
+                    }
+
+                    if (mat[j,i] < 0)
+                    {
+                        negative++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Main Diagonal:");
+            foreach (int num in mainD)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Negative numbers = " + negative);
 
         }
     }
